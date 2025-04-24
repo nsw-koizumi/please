c8ba086bc4a39a3894553653eedf9d4a556c71ac
nsw-koizumi <takahiro.koizumi@nsw.co.jp>
2025-04-24 17:04:11 +0900

    initial commit

---
+++ sample_notebook_CLI.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+# SparkSessionの作成
+spark = SparkSession.builder.getOrCreate()
+
+# COMMAND ----------
+
+# スキーマの定義
+schema = StructType([
+  StructField('CustomerID', IntegerType(), False),
+  StructField('FirstName', StringType(), False),
+  StructField('LastName', StringType(), False)
+])
+
+# COMMAND ----------
+
+# サンプルデータの作成
+data = [
+  [1000, '山田', '太郎'],
+  [1001, '鈴木', '花子'],
+  [1002, '佐藤', '次郎']
+]
+
+# DataFrameの作成と表示
+customers = spark.createDataFrame(data, schema)
+customers.show()
